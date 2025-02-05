27e69ae7ff3750f40f070ba5d27803708ba831f8
Ese-oghene <oghenee08@gmail.com>
2025-02-05 17:23:12 +0100

    first commit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideDisplayComponent } from './home/slide-display/slide-display.component';
+import { LoginComponent } from './home/login/login.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { SignupComponent } from './home/signup/signup.component';
+import { ContactPageComponent } from './home/contact-page/contact-page.component';
+import { AboutPageComponent } from './home/about-page/about-page.component';
+import { TestimonialPageComponent } from './home/testimonial-page/testimonial-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideDisplayComponent,
+    LoginComponent,
+    FooterComponent,
+    SignupComponent,
+    ContactPageComponent,
+    AboutPageComponent,
+    TestimonialPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/testimonial-page/testimonial-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonial-page',
+  templateUrl: './testimonial-page.component.html',
+  styleUrl: './testimonial-page.component.css'
+})
+export class TestimonialPageComponent {
+
+}
