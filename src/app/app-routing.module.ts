9ad480478d432025f91813310ca8078dc23b6ad5
Ese-oghene <oghenee08@gmail.com>
2025-02-06 14:00:47 +0100

    2nd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SlideDisplayComponent } from './home/slide-display/slide-display.component';
+import { LoginComponent } from './home/login/login.component';
+import { SignupComponent } from './home/signup/signup.component';
+import { ContactPageComponent } from './home/contact-page/contact-page.component';
+import { AboutPageComponent } from './home/about-page/about-page.component';
+import { TestimonialPageComponent } from './home/testimonial-page/testimonial-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideDisplayComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'testimonials', component: TestimonialPageComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
